65778920a195d858540ef615610e0821b83c4494
Alpadery <muhammad.alpadery@gmail.com>
2024-10-10 20:44:14 +0700

    Initial Commit 1

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 #page setup
-about_page = st.Page(
+about_page = st.page(
     page = "views/about_CM.py",
     title = 'Class Meeting Abu Dzar 2024',
     default = True,
 )
 
-project_1_page = st.Page(
+project_1_page = st.page(
     page = "views/Lomba_Estafet.py",
     title = "Lomba Estafet",
     icon = '👌',
