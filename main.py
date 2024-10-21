69d8fad3de21e7be5343d9fac68814b6301a49da
Alpadery <muhammad.alpadery@gmail.com>
2024-10-21 15:17:25 +0700

    main.py

--- main.py
+++ main.py
@@ -23,66 +23,80 @@ st.logo(sidebar_logo,
         size='large')
 
 if selected=='Class Meeting 2024':
-    st.title('Pusat Informasi Class Meeting Abu Dzar 2024')
-    st.write('---')
-    col1, col2 = st.columns((2,1))
-    with col1:
-        st.header('A.  Tentang Class Meeting Abu Dzar')
-        st.write(
-            """
-            Class Meeting merupakan kegiatan yang berlangsung setelah kegiatan Sumatif Akhir Semester dilaksanakan. Kegiatan ini merupakan sebuah *refreshment* untuk peserta didik setelah mengikuti SAS.""")
-    
-    col1, col2 = st.columns((2.5,1))
-    with col1:
-        st.header('B.  Tujuan dan Manfaat Class Meeting')
-        st.write(
-            """
-            1. Ajang hiburan dan relaksasi pasca ujian.
-            2. Mengembangkan bakat dan minat.
-            3. Mempererat hubungan antar siswa.
-            4. Mencari bibit prestasi ekstrakulikuler.
-            5. Melatih mental dan daya saing.
-            6. Memberikan kesempatan berekspresi.
-            """)
+    with st.container():
+        st.title('Pusat Informasi Class Meeting Abu Dzar 2024')
+        col1, col2 = st.columns((2,1))
+        with col1:
+            st.header('A.  Tentang Class Meeting Abu Dzar')
+            st.write(
+                """
+                Class Meeting merupakan kegiatan yang berlangsung setelah kegiatan Sumatif Akhir Semester dilaksanakan. Kegiatan ini merupakan sebuah *refreshment* untuk peserta didik setelah mengikuti SAS.""")
         
-        st.write('**Berikut hasil dokumentasi kegiatan Class Meeting 2023**')
-        st.write('[Click Here >](https://www.youtube.com/live/ibEefMmPfrg?si=q_8Ii5MS1T8Kgdeq)')
+        st.write('---')
+        col1, col2 = st.columns((2.5,1))
+        with col1:
+            st.header('B.  Tujuan dan Manfaat Class Meeting')
+            st.write(
+                """
+                1. Ajang hiburan dan relaksasi pasca ujian.
+                2. Mengembangkan bakat dan minat.
+                3. Mempererat hubungan antar siswa.
+                4. Mencari bibit prestasi ekstrakulikuler.
+                5. Melatih mental dan daya saing.
+                6. Memberikan kesempatan berekspresi.
+                """)
+            
+            st.write('**Berikut hasil dokumentasi kegiatan Class Meeting 2023**')
+            st.write('[Click Here >](https://www.youtube.com/live/ibEefMmPfrg?si=q_8Ii5MS1T8Kgdeq)')
 
-    
-    col1, col2 = st.columns((2.1,1))
-    with col1:
-        st.subheader('C.  Jadwal Kegiatan Class Meeting 2024')
-        image=Image.open('jadwal.png')
-        st.image(image,
-                use_column_width=True)
-    
-    st.header('D. Lokasi Lomba')
-    st.subheader('**Lapangan Atas**')
-    st.write(
-        """
-        Lapangan atas ini nantinya akan digunakan untuk 3 jenislomba dengan jadwal yang berbeda.
-        Jadwal pertama digunakan untuk lomba basket, kemudian setelahnya digunakan untuk lomba Blind Bottle dan Chopstick Ball dalam waktu bersamaan.
-        """)
-    image=Image.open('lapangan_atas.png')
-    st.image(image, use_column_width=True)
+        
+        col1, col2 = st.columns((2.1,1))
+        with col1:
+            st.subheader('C.  Jadwal Kegiatan Class Meeting 2024')
+            image=Image.open('jadwal.png')
+            st.image(image,
+                    use_column_width=True)
+        
+        st.write('---')
+        col1, col2 = st.columns((1,1))
+        with col1:
+            st.header('D. Lokasi Lomba')
+            st.subheader('**Lapangan Atas**')
+            st.write(
+                """
+                Lapangan atas ini nantinya akan digunakan untuk 3 jenis lomba dengan jadwal yang berbeda.
+                Jadwal pertama digunakan untuk lomba basket, kemudian setelahnya digunakan untuk lomba Blind Bottle dan Chopstick Ball dalam waktu bersamaan.
+                """)    
 
-    st.subheader('**Lapangan Bawah**')
-    st.write(
-        """
-        Lapangan bawah ini digunakan untuk 2 jenis lomba yaitu lomba futsal untuk kelas Ikhwan dan lomba (?) untuk kelas Akhwat.
-        Lomba futsal dilaksanakan pada Selasa, 11 Desember 2024 dan Lomba (?) dilaksanakan pada Rabu, 13 Desember 2024.
-        """)
-    image=Image.open('lapangan_bawah.png')
-    st.image(image, use_column_width=True)
+        with col2:
+            image=Image.open('lapangan_atas.png')
+            st.image(image, use_column_width=True)
 
-    st.subheader('**Lapangan Depan Perpustakaan**')
-    st.write(
-        """
-        Lapangan ini adalah tempat untuk lomba Volly Sarung. Lomba ini menggunakan media air sehingga pelaksanaannya perlu berada di daerah yang tidak licin,
-        atau lapangan yang terbuat dari *paving block*.
-        """)
-    image=Image.open('depan_perpus.png')
-    st.image(image, use_column_width=True)
+        st.write('---')
+        col1, col2 = st.columns((1,1))
+        with col1:
+            st.subheader('**Lapangan Bawah**')
+            st.write(
+            """
+            Lapangan bawah ini digunakan untuk 2 jenis lomba yaitu lomba futsal untuk kelas Ikhwan dan lomba (?) untuk kelas Akhwat.
+            Lomba futsal dilaksanakan pada Selasa, 11 Desember 2024 dan Lomba (?) dilaksanakan pada Rabu, 13 Desember 2024.
+            """)
+        with col2:
+            image=Image.open('lapangan_bawah.png')
+            st.image(image, use_column_width=True)
+
+        st.write('---')
+        col1, col2 = st.columns((1,1))
+        with col1:
+            st.subheader('**Lapangan Depan Perpustakaan**')
+            st.write(
+            """
+            Lapangan ini adalah tempat untuk lomba Volly Sarung. Lomba ini menggunakan media air sehingga pelaksanaannya perlu berada di daerah yang tidak licin,
+            atau lapangan yang terbuat dari *paving block*.
+            """)
+        with col2:
+            image=Image.open('depan_perpus.png')
+            st.image(image, use_column_width=True)
 
 #TTG LOMBA CM
 if selected=='Lomba Class Meeting':
