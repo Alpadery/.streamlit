2aa77aad461cfb1cab33dd0617bb66924146ad7c
Alpadery <muhammad.alpadery@gmail.com>
2024-10-31 11:24:13 +0700

    main.py

--- main.py
+++ main.py
@@ -435,25 +435,27 @@ if selected=='Bagan Kelas Ikhwan':
     selected_item = st.selectbox("Pilih Jenis lomba", list(item_to_sheets.keys()))
 
         # Function to read the sheet and replace None with blank cells
+   
+
     def read_sheet(sheet_name):
-            sheet = workbook[sheet_name]
-            data = []
+        sheet = workbook[sheet_name]
+        data = []
 
-            for row in sheet.iter_rows(values_only=True):
-                # Clean the row by replacing None with blank cells
-                cleaned_row = [cell if cell is not None else "" for cell in row]
-                # Only append rows that have non-empty values
-                if any(cleaned_row):
-                    data.append(cleaned_row)
+        for row in sheet.iter_rows(values_only=True):
+            # Clean the row by replacing None with blank cells
+            cleaned_row = [cell if cell is not None else "" for cell in row]
+            # Only append rows that have non-empty values
+            if any(cleaned_row):
+                data.append(cleaned_row)
 
-            # Convert to DataFrame
-            df = pd.DataFrame(data)
+        # Convert to DataFrame
+        df = pd.DataFrame(data)
 
-            return df
+        return df
 
-        # Display the chosen item's related sheets
+    # Display the chosen item's related sheets
     if selected_item:
-            # Get the related sheets and link based on the selected item
+        # Get the related sheets and link based on the selected item
         related_info = item_to_sheets[selected_item]
         related_sheets = related_info['sheets']
             
@@ -464,16 +466,17 @@ if selected=='Bagan Kelas Ikhwan':
 
         # Create a full width container for the DataFrames
         st.write(f"### {related_sheets[0]}")
-        st.dataframe(df1, width=800)
+        st.dataframe(df1, use_container_width=True, hide_index=True)
 
         st.write(f"### {related_sheets[1]}")
-        st.dataframe(df2, width=1000)
+        st.dataframe(df2, use_container_width=True, hide_index=True)
             
         st.write(f"### {related_sheets[2]}")
-        st.dataframe(df3, width=1000)
+        st.dataframe(df3, use_container_width=True, hide_index=True)
 
         # Add link for the selected item
         st.markdown(f"[More about {selected_item}]({related_info['link']})")
+
 
 
 if selected=='Bagan Kelas Akhwat':
