b3d6fea85514043bc8471df7e12ef2ec2a883e36
Alpadery <muhammad.alpadery@gmail.com>
2024-11-03 06:47:40 +0700

    main.py

--- main.py
+++ main.py
@@ -299,7 +299,7 @@ if selected=='Leaderboard':
         styled_df = styled_df.highlight_max(color='gray', axis=0, subset=['Silver Medals'], props='font-weight: bold; background-color: rgba(169, 169, 169, 0.7);')
 
         # Display the styled DataFrame in Streamlit
-        st.dataframe(styled_df)
+        st.dataframe(styled_df, use_container_width=True)
     else:
         st.write(f"The winner will be announced soon, stay tuned!")
 
