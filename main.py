8fce3d0954467b8d2a9ed99361cda58a3789fad1
Alpadery <muhammad.alpadery@gmail.com>
2024-11-14 11:35:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ with st.sidebar:
 sidebar_logo=('Logo Sekolah Islam Abu Dzar-01.png')
 main_body_logo=('Icon Logo Yayasan-01 (1).png')
 image_path = "LOGO CM.png"  # Replace with your image path or URL
-st.sidebar.image(image_path, use_column_width=True)
+st.sidebar.image(image_path, use_container_width=True)
 
 st.logo(sidebar_logo,
         icon_image='Icon Logo Yayasan-01 (1).png',
@@ -58,7 +58,7 @@ if selected=='Class Meeting 2024':
             st.header('C.  Jadwal Kegiatan Class Meeting 2024', divider='grey')
             image=Image.open('Jadwal CM 24.png')
             st.image(image,
-                    use_column_width=True)
+                    use_container_width=True)
         
         
         st.header('D. Lokasi Lomba', divider='grey')
@@ -72,7 +72,7 @@ if selected=='Class Meeting 2024':
 
         with col2:
             image=Image.open('lapangan_atas.png')
-            st.image(image, use_column_width=True)
+            st.image(image, use_container_width=True)
 
         st.write('---')
         col1, col2 = st.columns((1,1))
@@ -84,7 +84,7 @@ if selected=='Class Meeting 2024':
             """)
         with col2:
             image=Image.open('lapangan_bawah.png')
-            st.image(image, use_column_width=True)
+            st.image(image, use_container_width=True)
 
         st.write('---')
         col1, col2 = st.columns((1,1))
@@ -98,7 +98,7 @@ if selected=='Class Meeting 2024':
             """)
         with col2:
             image=Image.open('depan_perpus.png')
-            st.image(image, use_column_width=True)
+            st.image(image, use_container_width=True)
 
 #TTG LOMBA CM
 if selected=='Lomba Class Meeting':
@@ -133,7 +133,7 @@ if selected=='Lomba Class Meeting':
     if selected_lomba == "Futsal":
         st.header('2. Futsal')
         image=Image.open('futsal.png')
-        st.image(image, use_column_width=True)
+        st.image(image, use_container_width=True)
         st.subheader('**Pengertian Olahraga Futsal**')
         st.write("""
                 **Futsal** adalah olahraga tim yang merupakan variasi dari sepak bola, dimainkan di lapangan kecil dan biasanya di dalam ruangan.
@@ -155,7 +155,7 @@ if selected=='Lomba Class Meeting':
     if selected_lomba == "Basket":
         st.header('3. Basket')
         image=Image.open('basket.png')
-        st.image(image, use_column_width=True)
+        st.image(image, use_container_width=True)
         st.subheader('**Pengertian Olahraga Basket**')
         st.write("""
                 **Basket** adalah olahraga tim yang melibatkan dua tim dengan lima pemain di lapangan masing-masing.
@@ -179,7 +179,7 @@ if selected=='Lomba Class Meeting':
     if selected_lomba == "Dodgeball":
         st.header('4. Dodgeball')
         image=Image.open('bolaberacun.png')
-        st.image(image, use_column_width=True)
+        st.image(image, use_container_width=True)
         st.subheader('**Pengertian Permainan Dodgeball**')
         st.write("""
                 **Dodgeball** merupakan permainan yang melibatkan dua tim, yaitu tim pelempar dan tim mangsa.
@@ -204,7 +204,7 @@ if selected=='Lomba Class Meeting':
     if selected_lomba == "Bomb Ballon":
         st.header('5. Bomb Ballon')
         image=Image.open('voli.png')
-        st.image(image, use_column_width=True)
+        st.image(image, use_container_width=True)
         st.subheader('**Pengertian Permainan Bomb Ballon**')
         st.write("""
                 **Lomba Bomb Ballon** adalah yang mirip dengan voli biasa, namun menggunakan sarung sebagai alat untuk melempar balon air yang berisi air.
@@ -226,7 +226,7 @@ if selected=='Lomba Class Meeting':
     if selected_lomba == "Blind Bottle":
         st.header('6. Blind Bottle')
         image=Image.open('blindbottle.png')
-        st.image(image, use_column_width=True)
+        st.image(image, use_container_width=True)
         st.subheader('**Pengertian Permainan Blind Bottle**')
         st.write("""
                 **Lomba *Blind Bottle*** adalah permainan yang menguji kerja sama tim, komunikasi, dan kepercayaan antar anggota tim.
@@ -249,7 +249,7 @@ if selected=='Lomba Class Meeting':
     if selected_lomba == "Chopstick Ball":
         st.header('7. Chopstick Ball')
         image=Image.open('chopstickball.png')
-        st.image(image, use_column_width=True)
+        st.image(image, use_container_width=True)
         st.subheader('**Pengertian Permainan Chopstick Ball**')
         st.write("""
                 **Lomba *chopstick ball*** adalah Permainan yang menguji ketangkasan dan kecepatan peserta dalam mengeluarkan bola dari suatu wadah atau area menggunakan sumpit.
@@ -283,31 +283,29 @@ if selected=='Leaderboard':
 
     leaderboard_df.sort_values(by=['Winner', 'Runner Up'], ascending=False, inplace=True)
 
-    # Function to color entire 'Gold Medal' and 'Silver Medal' columns with transparency
+    # WARNA TRANSPARAN
     def color_medals(df):
-        styles = pd.DataFrame('', index=df.index, columns=df.columns,)  # Create an empty DataFrame for styles
-        styles['Winner'] = 'background-color: rgba(255, 215, 0, 0.5)'  # Gold with 50% transparency
-        styles['Runner Up'] = 'background-color: rgba(192, 192, 192, 0.5)'  # Silver with 50% transparency
-        styles['PTS'] = 'background-color: rgba(255, 255, 255, 0.5)'  # Silver with 50% transparency
+        styles = pd.DataFrame('', index=df.index, columns=df.columns,)
+        styles['Winner'] = 'background-color: rgba(255, 215, 0, 0.5)'
+        styles['Runner Up'] = 'background-color: rgba(192, 192, 192, 0.5)'
+        styles['PTS'] = 'background-color: rgba(255, 255, 255, 0.5)'
         return styles
 
-    # Check if the DataFrame is not empty
+    # IF ELSE DATA KOSONG
     st.header('🎖️ Hasil Perlombaan Ikhwan')
     if not leaderboard_df.empty:
-        # Apply the styling to the DataFrame
+        # DATA STYLE
         styled_df = leaderboard_df.style.apply(color_medals, axis=None)
 
-        # Highlight the maximum values in the 'Gold Medal' and 'Silver Medal' columns
+        # MAX MIN COLOR
         styled_df = styled_df.highlight_max(color='darkorange', axis=0, subset=['Winner'], props='font-weight: bold; background-color: rgba(255, 140, 0, 0.7);')
         styled_df = styled_df.highlight_max(color='gray', axis=0, subset=['Runner Up'], props='font-weight: bold; background-color: rgba(169, 169, 169, 0.7);')
 
-        # Display the styled DataFrame in Streamlit
         st.dataframe(styled_df)
     else:
         st.write(f"The winner will be announced soon, stay tuned!")
 
     st.write('')
-
 
 # LEADERBOARD AKHWAT
     data = {
@@ -316,13 +314,11 @@ if selected=='Leaderboard':
     'Runner Up': [],
     'PTS':[]
     }
-
-    # Dataframe
     leaderboard_df = pd.DataFrame(data)
 
     leaderboard_df.sort_values(by=['Winner', 'Runner Up'], ascending=False, inplace=True)
 
-    # Function to color entire 'Gold Medal' and 'Silver Medal' columns with transparency
+    # WARNA TRANSPARAN
     def color_medals(df):
         styles = pd.DataFrame('', index=df.index, columns=df.columns)  # Create an empty DataFrame for styles
         styles['Winner'] = 'background-color: rgba(255, 215, 0, 0.5)'  # Gold with 50% transparency
@@ -330,17 +326,16 @@ if selected=='Leaderboard':
         styles['PTS'] = 'background-color: rgba(255, 255, 255, 0.5)'  # Silver with 50% transparency
         return styles
 
-    # Check if the DataFrame is not empty
+    # ELSE IF DATA KOSONG
     st.header('🎖️ Hasil Perlombaan Akhwat')
     if not leaderboard_df.empty:
-        # Apply the styling to the DataFrame
+        # DATA STYLE
         styled_df = leaderboard_df.style.apply(color_medals, axis=None)
 
-        # Highlight the maximum values in the 'Gold Medal' and 'Silver Medal' columns
+        # MAX MIN COLOR
         styled_df = styled_df.highlight_max(color='darkorange', axis=0, subset=['Winner'], props='font-weight: bold; background-color: rgba(255, 140, 0, 0.7);')
         styled_df = styled_df.highlight_max(color='gray', axis=0, subset=['Runner Up'], props='font-weight: bold; background-color: rgba(169, 169, 169, 0.7);')
 
-        # Display the styled DataFrame in Streamlit
         st.dataframe(styled_df)
     else:
         st.write(f"The winner will be announced soon, stay tuned!")
@@ -358,7 +353,7 @@ if selected=='Leaderboard':
     juarafase_df.sort_values(by=['juara fase'],ascending=False, inplace=True)
     if not juarafase_df.empty:
             image=Image.open('juarafase_i.png')
-            st.image(image, use_column_width=True)
+            st.image(image, use_container_width=True)
     else:
             st.write('*"Competition is not about winning or losing, but about learning and growth"*  **-Brian Herbert**')
             
@@ -375,7 +370,7 @@ if selected=='Leaderboard':
     juarafase_df.sort_values(by=['juara fase'],ascending=False, inplace=True)
     if not juarafase_df.empty:
             image=Image.open('juarafase_i.png')
-            st.image(image, use_column_width=True)
+            st.image(image, use_container_width=True)
     else:
             st.write('*"The lessons of competition are lessons for life"*  **-Robert Kennedy**')
 
@@ -383,28 +378,26 @@ if selected=='Leaderboard':
     st.header(' Finalis Ikhwan Class Meeting 2024')
 
     image=Image.open('finalis_i.png')
-    st.image(image, use_column_width=True)
+    st.image(image, use_container_width=True)
         
     st.write("")
         
     st.header('Finalis Akhwat Class Meeting 2024')
 
     image=Image.open('finalis_i.png')
-    st.image(image, use_column_width=True)
+    st.image(image, use_container_width=True)
     
 if selected=='Pertandingan Ikhwan':
    
     st.title('Informasi Pertandingan Ikhwan Class Meeting Abu Dzar 2024')
     st.write("---")
 
-    # Specify the path to your Excel file
-
+    # FILE PATH
     file_path = "Jadwal Ikhwan CM 24 (1).xlsx"  # Change this to your actual file path
 
-    # Load the workbook
     workbook = load_workbook(filename=file_path, data_only=True)
 
-    # Mapping of items to their corresponding sheets and links
+    # MAPPING SHEET
     item_to_sheets = {
         'Estafet': {
             'sheets': ['(Ikhwan) Estafet A','(Ikhwan) Estafet B','(Ikhwan) Estafet C'],
@@ -436,25 +429,26 @@ if selected=='Pertandingan Ikhwan':
         }
     }
 
-    # Select box for user to choose an item
+    # SELECT BOX
     selected_item = st.selectbox("Pilih Jenis lomba", list(item_to_sheets.keys()))
 
-    # Function to read sheets and return cleaned DataFrame
+    # FUNCTION TO READ THE SHEETS
     def read_sheet(sheet_name):
         sheet = workbook[sheet_name]
         data = [[cell if cell is not None else "" for cell in row] for row in sheet.iter_rows(values_only=True)]
-        return pd.DataFrame(data).dropna(how='all')  # Drop empty rows
+        return pd.DataFrame(data).dropna(how='all')
     
-    # Display related sheets for selected item
+    # DISPLAY THE DATA
     if selected_item:
         related_info = item_to_sheets[selected_item]
         related_sheets = related_info['sheets']
         st.header(f"*Upcoming Match!*")
         st.write("""Temukan informasi lengkap mengenai pertandingan berikutnya beserta jadwal waktunya dengan melihat bagan pertandingan di bawah ini.""")
-        # Link for more information
+        
+        # LINK
         st.markdown(f"[Lihat Bagan Pertandingan {selected_item}]({related_info['link']})")
         
-        # Read and display DataFrames in a single column
+        st.write("---")
         st.header(f"Klasemen Lomba {selected_item}")
         for sheet in related_sheets:
             df = read_sheet(sheet)
@@ -468,14 +462,12 @@ if selected=='Pertandingan Akhwat':
     st.title('Informasi Pertandingan Akhwat Class Meeting Abu Dzar 2024')
     st.write("---")
 
-    # Specify the path to your Excel file
-
+    # FILE PATH
     file_path = "Jadwal Akhwat CM 24.xlsx"  # Change this to your actual file path
 
-    # Load the workbook
     workbook = load_workbook(filename=file_path, data_only=True)
 
-    # Mapping of items to their corresponding sheets and links
+    # MAPPING SHEETS
     item_to_sheets = {
         'Estafet': {
             'sheets': ['(Akhwat) Estafet A','(Akhwat) Estafet B','(Akhwat) Estafet C'],
@@ -507,10 +499,10 @@ if selected=='Pertandingan Akhwat':
         }
     }
 
-    # Select box for user to choose an item
+    # SELECT BOX
     selected_item = st.selectbox("Pilih Jenis lomba", list(item_to_sheets.keys()))
 
-    # Function to read sheets and return cleaned DataFrame
+    # FUNCTION TO READ THE SHEETS
     def read_sheet(sheet_name):
         sheet = workbook[sheet_name]
         data = [[cell if cell is not None else "" for cell in row] for row in sheet.iter_rows(values_only=True)]
@@ -522,10 +514,10 @@ if selected=='Pertandingan Akhwat':
         related_sheets = related_info['sheets']
         st.header(f"*Upcoming Match!*")
         st.write("""Temukan informasi lengkap mengenai pertandingan berikutnya beserta jadwal waktunya dengan melihat bagan pertandingan di bawah ini.""")
-        # Link for more information
+        # LINK
         st.markdown(f"[Lihat Bagan Pertandingan {selected_item}]({related_info['link']})")
         
-        # Read and display DataFrames in a single column
+        st.write("---")
         st.header(f"Klasemen Lomba {selected_item}")
         for sheet in related_sheets:
             df = read_sheet(sheet)
