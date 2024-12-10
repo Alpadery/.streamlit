985f62a045310dbd2327020ba3f6f1053f5c3490
Alpadery <muhammad.alpadery@gmail.com>
2024-12-10 14:14:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -412,7 +412,7 @@ if selected=='Pertandingan Ikhwan':
     st.write("---")
 
     # FILE PATH
-    file_path = "Jadwal Ikhwan CM 24 (3).xlsx"  # Change this to your actual file path
+    file_path = "Jadwal Ikhwan CM 24 (4).xlsx"  # Change this to your actual file path
 
     workbook = load_workbook(filename=file_path, data_only=True)
 
